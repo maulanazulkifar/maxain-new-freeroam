d860ff21b83f4b7638ac486dbf1927a316b3026b
maul <maulana.zulkifar@oppo-aed.id>
2026-09-05 10:01:35 +0700

    feat: add custom head model 046 for male and female freemode characters

---
+++ [standalone]/custom_head/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'stream/mp_m_freemode_01.ymt',
+    'stream/mp_f_freemode_01.ymt'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/mp_m_freemode_01.ymt'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/mp_f_freemode_01.ymt'
